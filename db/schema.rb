04027f55bc038d18fce18c14b8c758963c7b2fb9
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-03 12:00:05 -0400

    Add sign up page
    * Created user model and migration
    * Added sign up view
    * Added users controller
    * Added link to layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140701193405) do
+ActiveRecord::Schema.define(version: 20140703153457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20140701193405) do
     t.text     "description"
     t.string   "url"
     t.integer  "gallery_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
