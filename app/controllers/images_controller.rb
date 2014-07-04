747dec991c0861942a29724ed75f384fc4bb2959
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-04 11:39:34 -0400

    Refactor redundant path syntax

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -14,7 +14,7 @@ class ImagesController < ApplicationController
     @image = Image.new(image_params)
     @image.gallery_id = params[:gallery_id]
     if @image.save
-      redirect_to gallery_path(@image.gallery_id)
+      redirect_to @gallery
     else
       render :new
     end
@@ -29,7 +29,7 @@ class ImagesController < ApplicationController
     @gallery = Gallery.find(params[:gallery_id])
     @image = Image.find(params[:id])
     if @image.update(image_params)
-      redirect_to gallery_path(@image.gallery_id)
+      redirect_to @gallery
     else
       render :edit
     end
@@ -40,7 +40,7 @@ class ImagesController < ApplicationController
     gallery = image.gallery
     image.destroy
 
-    redirect_to gallery_path(gallery.id)
+    redirect_to gallery
   end
 
   private
