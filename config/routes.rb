a7c099e555eaded8eeba2e07bd0bcdb2739a048e
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-07 11:55:51 -0400

    Add home page with sign in / sign up forms
    * Created homes#new
    * Extracted sign_in_form to partial
    * Extracted sign_up_form to partial

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root to: "galleries#index"
+  root to: "homes#show"
 
   resource :session, only: [:new, :create, :destroy]
 
