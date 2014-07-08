4cf4e5ee2deeba456341dfe1dac8087ddeff7598
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-08 14:36:51 -0400

    Update nav heading and redirects
    * Show links to "My Galleries" and "All Galleries" when signed in
    * Redirect to galleries#show after signing in (instead of user#show)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = authenticate_session(session_params)
 
     if sign_in(user)
-      redirect_to user
+      redirect_to galleries_path
     else
       render :new
     end
