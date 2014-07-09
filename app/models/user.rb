162e3149d619b344c6cba2a9f82c5113c8c4b790
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-09 14:53:40 -0400

    Add uniqueness validation to user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   has_many :group_memberships
   has_many :groups, through: :group_memberships
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 end
