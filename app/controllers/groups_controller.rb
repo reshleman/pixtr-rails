a5c06f7031f69002a1619aec8604c6b0ec8c2f31
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-09 11:43:45 -0400

    Users are members of groups
    * Update groups#create to add current user as a member when creating a new group
    * Display a user's group memberships on the user show page
    * Refactor nav header for clarity

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,8 +9,10 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    group_membership = GroupMembership.new(group_membership_params(@group))
 
     if @group.save
+      group_membership.save
       redirect_to groups_path
     else
       render :new
@@ -23,5 +25,9 @@ class GroupsController < ApplicationController
     params.
       require(:group).
       permit(:name, :description)
+  end
+
+  def group_membership_params(group)
+    { user: current_user, group: group }
   end
 end
