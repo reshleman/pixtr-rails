9a8890c83d177f233bc879991418fa552ca67fd3
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-10 10:41:11 -0400

    Show flash messages when joining or leaving a group

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -3,13 +3,13 @@ class GroupMembershipsController < ApplicationController
     group = Group.find(params[:id])
     current_user.join(group)
 
-    redirect_to group
+    redirect_to group, alert: "You've joined this group. Welcome!"
   end
 
   def destroy
     group = Group.find(params[:id])
     current_user.leave(group)
 
-    redirect_to group
+    redirect_to group, alert: "You've left the group. Bye!"
   end
 end
