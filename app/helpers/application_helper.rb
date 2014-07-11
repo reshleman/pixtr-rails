ea4abb02fbb16b8ce5204d7f6133d8b583b043e3
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-11 13:59:10 -0400

    Add application helper for unordered lists
    * Helper method renders partials in a ul for each item in a collection
    * Refactor views to use this helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def render_list(collection)
+    content_tag(:ul) do
+      raw collection_as_lis(collection).join
+    end
+  end
+
+  private
+
+  def collection_as_lis(collection)
+    collection.map do |item|
+      content_tag(:li) { render item }
+    end
+  end
 end
